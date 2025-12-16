dceddf4adf85fecce89a9af7708752874905e085
katuen <rikusama112@gmail.com>
2025-12-17 04:18:50 +0900

    category
    データの受け渡しを修正 (add 関数)
    
    Category.create の引数を修正しました。スペルミスのある classfication の値を、データベースの正しい項目 classification に変換して渡すようにしました。
    
    編集機能のバグ修正 (edit 関数)
    
    URLの変数名 <int:product_id> と関数の引数 product_id を一致させました（以前は category_id と書いてあり、エラーの原因でした）。
    
    存在しない category.price への書き込みを削除し、正しく category.classification を更新するようにしました。

--- routes/category.py
+++ routes/category.py
@@ -1,18 +1,15 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-
 from models import Category
 
 # Blueprintの作成
 product_bp = Blueprint('category', __name__, url_prefix='/categories')
 
 
-
 @product_bp.route('/')
 def list():
-
     category = Category.select()
-    return render_template('category_list.html', title='カテゴリー一覧', items=category)
-
+    # 費目一覧を表示
+    return render_template('category_list.html', title='費目一覧', items=category)
 
 
 @product_bp.route('/add', methods=['GET', 'POST'])
@@ -22,24 +19,36 @@ def add():
     if request.method == 'POST':
         name = request.form['name']
 
-        classfication = request.form['classfication']
-        Category.create(name=name, classfication=classfication)
+        # HTMLフォームのname属性 "classfication" (スペルミス) を受け取る
+        classfication_val = request.form['classfication']
+        
+        # データベースの正しい列名 "classification" に保存する
+        # "1"なら収入(True), "0"なら支出(False)として変換して保存
+        is_income = (classfication_val == '1')
+        
+        Category.create(name=name, classification=is_income)
+        
         return redirect(url_for('category.list'))
     
     return render_template('category_add.html')
 
 
+# HTMLのURL指定に合わせて <int:product_id> を受け取るように修正
 @product_bp.route('/edit/<int:product_id>', methods=['GET', 'POST'])
-def edit(category_id):
-    category = Category.get_or_none(Category.id == category_id)
+def edit(product_id):
+    # 受け取った product_id を使ってデータを取得
+    category = Category.get_or_none(Category.id == product_id)
     if not category:
         return redirect(url_for('category.list'))
 
     if request.method == 'POST':
         category.name = request.form['name']
-        category.price = request.form['classfication']
+        
+        # 編集時も同様に "classfication" を受け取り "classification" に保存
+        classfication_val = request.form['classfication']
+        category.classification = (classfication_val == '1')
+        
         category.save()
         return redirect(url_for('category.list'))
 
     return render_template('category_edit.html', category=category)
-
